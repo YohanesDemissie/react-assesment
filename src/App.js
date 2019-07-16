3b8b0211d3cc5d83f590d822bf0626b64218da86
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-07-15 18:19:56 -0700

    fixed warnings of nested elements in jsx

--- src/App.js
+++ src/App.js
@@ -26,16 +26,14 @@ class App extends React.Component {
       <div>
         <p>
           Please implement a text box with autocomplete suggestions like the screenshot below.
-      </p>
-        <p>
-          <h2>Notes:</h2>
-          <ul>
-            <li>Please use the "<b>Fork</b>" button to start your work, so that you could have your private dedicated url. And please send the url back to the recruiter once you are done. </li>
-            <li>Please use the provided mock function as the suggestion API. Do not modify it.</li>
-            <li>Don't simply use a finished library component providing similar functionalities.</li>
-            <li>Please implement the control using React.js.</li>
-          </ul>
         </p>
+        <h2>Notes:</h2>
+        <ul>
+          <li>Please use the "<b>Fork</b>" button to start your work, so that you could have your private dedicated url. And please send the url back to the recruiter once you are done. </li>
+          <li>Please use the provided mock function as the suggestion API. Do not modify it.</li>
+          <li>Don't simply use a finished library component providing similar functionalities.</li>
+          <li>Please implement the control using React.js.</li>
+        </ul>
 
         <img src="https://vafyqw.bn1301.livefilestore.com/y4m95aNR29WvCX2FZiHr6lGgyYGqWnJp5MzPFHJAQQT-QAwZ08XCwI-ehTJNfGSyuZ7Z0giumulisPaqzQzOftlmFmb6ZAuBK_jDt2KCczcpk9xkKUOV73gWRgvYcDbH97Uzjm4hDW0-NPZ9TMX4Sih9AnCknlZ1Z7i_y16fNAk8SfV0Hn8Y-DjaxrIm7BTVWJs9FgjPdrY5Fgr0V1b5fpBcw?width=660&height=319&cropmode=none" alt="broken"
           style={imgStyle} />
